4f81ee82232a568f087293620d8b7e0c65d65ede
HankBedford <Zachary.Lynch1337@gmail.com>
2019-01-28 11:54:31 -0800

    Inheritence in python

--- classes.py
+++ classes.py
@@ -7,11 +7,21 @@ class myClass():
     def method2(self, someString):
         print("myClass method2" + someString)
 
+class myClass2(myClass):
+    def method(self):
+        myClass.method(self)
+        print("method1 anotherClass")
+
+    def method2(self, someString):
+        print("anotherClass method2" )
 
 def main():
     c = myClass()
+    c2 = myClass2()
     c.method()
     c.method2(" called back!")
+    c2.method()
+    c2.method2(" Another callback")
 
 
 if __name__ == "__main__":
